71f6c212f5ac66cc52504b0aff7e5f51b0890c73
Jan.Pilch <jan.pilch@atipera.com>
2025-04-28 16:06:31 +0200

    Initial commit

---
+++ src/test/kotlin/org/acme/GreetingResourceTest.kt
@@ -0,0 +1,44 @@
+package org.acme
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.eclipse.microprofile.config.ConfigProvider
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class GreetingResourceTest {
+
+    companion object {
+        const val BATCH_QUEUE_PROP_V1 = "batch-queue.queue-url"
+        const val BATCH_QUEUE_PROP_V2 = "batch.queue-url"
+    }
+
+    @Test
+    fun testHelloEndpoint() {
+        given()
+          .`when`().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(`is`("Hello from Quarkus REST"))
+    }
+
+
+    @Test
+    fun testingPropertyValues() {
+        val actualQueueUrlV1 = getRawValue(BATCH_QUEUE_PROP_V1)
+        val actualQueueUrlV2 = getRawValue(BATCH_QUEUE_PROP_V2)
+
+        println("actualQueueUrlV1: $actualQueueUrlV1")
+        println("actualQueueUrlV2: $actualQueueUrlV2")
+
+        assert(actualQueueUrlV1 == actualQueueUrlV2)
+    }
+
+    private fun getRawValue(prop: String): String {
+        return ConfigProvider.getConfig()
+            .getConfigValue(prop)
+            .rawValue
+    }
+
+}
